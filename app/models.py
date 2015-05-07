710de120a3fbe80e9f4adfe5bab318dbb166ee80
Alex <alex.makijokela@gmail.com>
2015-05-06 18:54:40 -0700

    add gitignore, change models

--- app/models.py
+++ app/models.py
@@ -3,22 +3,24 @@ import datetime
 
 
 
-class User(db.Model):  #design descision! What characteristics do I want the users to have?
+class User(db.Model):  
     id = db.Column(db.Integer, primary_key = True)
     phonenum = db.Column(db.String(20), index = True)
     pw_hash = db.Column(db.String(255))
     join_datetime = db.Column(db.DateTime())
     
-class Arc(db.Model):
+class Question(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	reminder_text = db.Column(db.String(255))
+	question_text = db.Column(db.String(255))
 	created_datetime = db.Column(db.DateTime())
 	daily_time = db.Column(db.Time())
+	state = db.Column(db.Integer())
 
-	def __init__(self, reminder_text):
-		
+	def __init__(self, question_text):
+		self.question_text=question_text
 
-class Message(db.Model):
+
+class Response(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	sent_datetime = db.Column(db.DateTime())
 	reply = db.Column(db.String(255))
